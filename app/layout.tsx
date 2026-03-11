e2fa177d73203f3ae9607ba80ff980ccdc538812
TerraOps <admin@terra-net.uk>
2026-03-11 23:50:29 +0000

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,27 @@
-import "styles/tailwind.css"
+import "styles/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function RootLayout({ children }: { children: React.ReactNode } RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   )
 }
