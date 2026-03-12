18ee33f6990e99aac1e14355ead28d649f9ce36b
TerraOps <admin@terra-net.uk>
2026-03-12 00:04:43 +0000

    Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import "styles/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 
-export default function RootLayout({ children }: { children: React.ReactNode } RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode } R
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
